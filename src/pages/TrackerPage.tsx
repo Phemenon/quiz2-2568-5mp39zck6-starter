8304b85050395ff432efcda8333e69afa0153085
Phenomenon <xiii15499@gmail.com.com>
2025-09-08 11:34:18 +0700

    quiz-phase2

--- src/pages/TrackerPage.tsx
+++ src/pages/TrackerPage.tsx
@@ -1,5 +1,14 @@
-import { useState } from "react";
-import { Button, Stack, Title, Divider, Container } from "@mantine/core";
+import { useState, useEffect } from "react";
+import {
+  Button,
+  Stack,
+  Title,
+  Divider,
+  Container,
+  Card,
+  Group,
+  Text,
+} from "@mantine/core";
 import { v4 as uuidv4 } from "uuid";
 import AddExpenseModal from "../components/Modal";
 
@@ -15,10 +24,23 @@ export default function ExpenseTracker() {
   const [expenses, setExpenses] = useState<Expense[]>([]);
   const categories = ["Food", "Transport", "Entertainment"];
 
+  useEffect(() => {
+    const stored = localStorage.getItem("my-expense");
+    if (stored) {
+      const parsed = JSON.parse(stored);
+      setExpenses(parsed);
+    }
+    setExpenses(expenses);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("my-expense", JSON.stringify(expenses));
+  }, [expenses]);
+
   const setExpense = (
     name: string,
     amount: number | string,
-    category: string | null,
+    category: string | null
   ) => {
     const newExpenses: Expense = {
       id: uuidv4(),
@@ -27,6 +49,10 @@ export default function ExpenseTracker() {
       category,
     };
     setExpenses((prev) => [...prev, newExpenses]);
+  };
+
+  const deleteExpense = (expenseId: string) => {
+    setExpenses((prev) => prev.filter((t) => t.id !== expenseId));
   };
 
   return (
@@ -41,11 +67,28 @@ export default function ExpenseTracker() {
         onClose={() => setOpened(false)}
         onAdd={setExpense}
       />
-
       <Divider my="md" />
       {/* Type additional total cost here. */}
-      <Title order={4}>Total cost: {} Baht</Title>
-      <Stack my="sm">{/* Type additional text here. */}</Stack>
+      <Title order={4}>Total cost: {  } Baht</Title>
+      <Stack my="sm">
+        {expenses.map((expense) => (
+          <Card withBorder shadow="sm" radius="md" mb="sm" key={expense.id}>
+            <Group justify="space-between" align="flex-start">
+              <Stack>
+                <Text fw={600} size="lg">
+                  {expense.name}
+                </Text>
+                <Text size="sm" c="dimmed">
+                  {expense.amount}
+                </Text>
+                <Text size="xs" c="gray">
+                  {expense.category}
+                </Text>
+              </Stack>
+            </Group>
+          </Card>
+        ))}
+      </Stack>
 
       <Divider my="md" />
       {/* Type additional card here. */}
